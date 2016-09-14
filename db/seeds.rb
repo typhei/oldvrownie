adde36a20423484be498f4149e0ee8da9913ea5f
Hashimoto <hashimoto@dl.kuis.kyoto-u.ac.jp>
2016-09-15 03:37:00 +0900

    image path changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -103,7 +103,7 @@
 @page.number = 30
 @page.title = "バーチャルエロの最先端「アダルトVRエキスポ2016」潜入レポ｜ニフティニュース"
 @page.url = "https://news.nifty.com/article/technology/mobile/12125-291289/"
-@page.image = "/images//assets//assets/thumb-12125-291289-technology.jpg"
+@page.image = "/images/thumb-12125-291289-technology.jpg"
 @page.body = "先日行われた「アダルトVRエキスポ2016」の模様をお届けしたいと思う。筆者の友人であり、アダルト業界においては先輩であるみくら天狗氏の案内で、最先端のアダルトVR（バーチャルリアリティ）を体験してき…"
 @page.site_name = "ニフティニュース"
 @page.date = "2016/09/13"
@@ -113,7 +113,7 @@
 @page.number = 29
 @page.title = "紳士的にもこれはラク、無線でDVDとスマホをつなぎVR化【アダルトご注意】"
 @page.url = "http://ascii.jp/limit/group/ida/elem/000/001/227/1227571/"
-@page.image = "/images//assets//assets/00a_ogp.jpg"
+@page.image = "/images/00a_ogp.jpg"
 @page.body = "アダルトDVDを寝転がって手軽に見たい！という紳士必見。スマホのVRヘッドセットでアダルトDVD動画を楽しむ方法を教えます。"
 @page.site_name = "ASCII.jp"
 @page.date = "2016/09/13"
@@ -123,7 +123,7 @@
 @page.number = 28
 @page.title = "【職場閲覧注意】アダルトVRに見る「受動的コンテンツ」の未来（ギズモード・ジャパン） - グノシー"
 @page.url = "https://gunosy.com/articles/RsicJ"
-@page.image = "/images//assets//assets/115dcfeb887e81cf56d8554cfeebb1bc_content.jpg"
+@page.image = "/images/115dcfeb887e81cf56d8554cfeebb1bc_content.jpg"
 @page.body = "マグロstyleでも、どこまで脳を気持ちよくさせられるのか。
 開催するも途中で中止。企画するも地域住民の要望で会場変更。そんな逆境に負けじと、8月27日に都内某所で開催された「アダルトVRエキスポ2016」にお邪魔してきました。
 2016年はVR元年といわれています。確かに、個人でも入手しやすい機材がそろってきましたし、コンテンツも増えてきました。そして新しいコンテンツビューワーは18禁なコンテン"
@@ -135,7 +135,7 @@
 @page.number = 27
 @page.title = "幕張でVR美少女を体感できる！ サイバーエージェントより『オルタナティブガールズ』出展決定【TGS2016】 [ファミ通App]"
 @page.url = "http://app.famitsu.com/20160912_831906/"
-@page.image = "/images//assets//assets/a058519b6b42ec72a12c0cb7c455f85d-1-300x300.png"
+@page.image = "/images/a058519b6b42ec72a12c0cb7c455f85d-1-300x300.png"
 @page.body = "サイバーエージェントは、スマホアプリ『オルタナティブガールズ』を、“東京ゲームショウ2016へ出展することを発表した。"
 @page.site_name = "ファミ通App"
 @page.date = "2016/09/13"
@@ -145,7 +145,7 @@
 @page.number = 26
 @page.title = "カスメVRと空気嫁　初体験でベッドから落下して顔面打った話 - 内向人種の行きつく先は"
 @page.url = "http://wannabedaruma.hatenablog.jp/entry/2016/09/13/%E3%82%AB%E3%82%B9%E3%83%A1VR%E3%81%A8%E7%A9%BA%E6%B0%97%E5%AB%81_%E5%88%9D%E4%BD%93%E9%A8%93%E3%81%A7%E3%83%99%E3%83%83%E3%83%89%E3%81%8B%E3%82%89%E8%90%BD%E4%B8%8B%E3%81%97%E3%81%A6%E9%A1%94"
-@page.image = "/images//assets//assets/20160830172625.jpg"
+@page.image = "/images/20160830172625.jpg"
 @page.body = "VRでカスタムメイド3D2をしながらラブボディーakiに挿入してたらベッドから落ちた話をさせてください。前に書いたカスメVR記事で言った通り、ラブボディーakiを買ったのです。 wannabedaruma.hatenablog.jp 断っておきたいのは、カスメもラブボディーakiも非常に良いものであり、この2つの合わせ技ともなると、それはもうオナニーではなくセックスであるといえるほどに素晴らしいものであるということ。にもかかわらず、というか、だからこそなのでしょうか、僕の初体験が痛みを伴う結果になったのは…… 多分僕が童貞だからいけなかったんだと思います。"
 @page.site_name = "内向人種の行きつく先は"
 @page.date = "2016/09/13"
@@ -155,7 +155,7 @@
 @page.number = 25
 @page.title = "全男子必見！美女とひとつ屋根の下で暮らせるPSVR対応ゲーム『しあわせ荘の管理人さん。』ティザーサイトオープン [ファミ通App]"
 @page.url = "http://app.famitsu.com/20160908_828522/"
-@page.image = "/images//assets//assets/4070a6d772410143b9a9bd3944d97117-300x300.jpg"
+@page.image = "/images/4070a6d772410143b9a9bd3944d97117-300x300.jpg"
 @page.body = "2016年9月8日より、プレイステーション4（PS4）専用、プレイステーションVR（PSVR）対応ソフト『しあわせ荘の管理人さん。』のティザーサイトが公開された。"
 @page.site_name = "ファミ通App"
 @page.date = "2016/09/13"
@@ -165,7 +165,7 @@
 @page.number = 24
 @page.title = "【体験レポ】微弱な電気を流して“VR酔い”を克服できる？ | Mogura VR"
 @page.url = "http://www.moguravr.com/vr-nausea-cedec2016/"
-@page.image = "/images//assets//assets/image00302.jpg"
+@page.image = "/images/image00302.jpg"
 @page.body = "2016年8月24日から開催されていたCEDEC2016では、VR関連の技術やデモが展示される「VR NOW!」コーナーが設置されました。 数回に分けて、興味深かった展示を紹介していきます。 今回紹介するのは、『前庭電気刺激』。大阪大学准教授の安藤英由樹氏、青山一真氏、樋口大貴氏の3名による展示です。内耳の奥にある”前庭”に微弱な電気を流すことで、加速度感覚や角速度を感じさせる技術を用いて、ヘッドマウントディスプレイ（ＨＭＤ）の映像にさらに臨場感を加味するというものです。  展示されていたデモでは、同意書にサインをしてから、耳の後ろと、頬に心電図用のパッドを付けます。そのパッドを通して微弱な電流を流して、CGのジェットコースターに乗っているVRを体験。コースの半分を電流を流さず体験し、半分過ぎたところで、体験者自らスイッチを押して電流を流し体感を比較します。 ジェットコースターをVRで体験すると、実際に感じているはずのない重力を感じます。このデモでは、電流を流して角速度や加速を体が感じることで感覚を相殺し、酔いにくくなる、という仮説に基づいて作成されたものになります。 体験中にスイッチを入れると、顔の下半分が痺れ、口の中に塩味と酸味を感じました。その印象が強すぎて、流す前と後の比較を感じる余裕はなかった、というのが実際のところです。 ヘッドマウントディスプレイを外した状態で、左右だけの傾きを感じることができるかを体感させてもらいました。両足を閉じた状態で直立し電気を流してもらうと、自分の体でありながら、勝手に左に折れ曲がっていきました。 展示では、まだ研究段階ではありますが、「平衡感覚を操作される」という感覚を味わうことになりました。 この『前庭電気刺激』を使ってVR酔いを防ぐ技術は、2016年3月サムスンがEntrim 4Dというヘッドホンのプロトタイプを発表するなど注目を集めています。 https://www.youtube.com/watch?v=yZ_G_zbObWU"
 @page.site_name = "MoguraVR"
 @page.date = "2016/09/13"
@@ -175,7 +175,7 @@
 @page.number = 23
 @page.title = "『プレイステーションVR』に、８本のゲームを体験できる無料のデモディスクが付属決定！ジャンルが豊富でどれも楽しそうｗｗｗ：はちま起稿"
 @page.url = "http://blog.esuteru.com/archives/20001761.html"
-@page.image = "/images//assets//assets/6b398db402cd8eba7992c9369442403c.png"
+@page.image = "/images/6b398db402cd8eba7992c9369442403c.png"
 @page.body = "『プレイステーションVR』に、８本のゲームを体験できる無料のデモディスクが付属決定！ジャンルが豊富でどれも楽しそうｗｗｗの記事詳細。国内最大のエンタメまとめメディアサイト。ゲーム情報、アニメや漫画、時事ネタなど、たくさんのネタを面白おかしく紹介します。"
 @page.site_name = "はちま起稿"
 @page.date = "2016/09/13"
@@ -185,7 +185,7 @@
 @page.number = 22
 @page.title = "名作『クーロンズゲート』がPSVRで復活！『クーロンズゲートVR 朱雀』のクラウドファンディングがスタート！！ : ゲームかなー"
 @page.url = "http://blog.gamekana.com/archives/8685358.html"
-@page.image = "/images//assets//assets/7266f25c.jpg"
+@page.image = "/images/7266f25c.jpg"
 @page.body = "「クーロンズゲート」復活"
 @page.site_name = "ゲームかなー"
 @page.date = "2016/09/13"
@@ -195,7 +195,7 @@
 @page.number = 21
 @page.title = "北米版『プレイステーションVR』には18本収録の体験版ディスクが同梱！なんと『バイオハザード7（キッチン）』も！日本もたのんますよ・・・ : ゲームかなー"
 @page.url = "http://blog.gamekana.com/archives/8685237.html"
-@page.image = "/images//assets//assets/65afc064.png"
+@page.image = "/images/65afc064.png"
 @page.body = "プレイステーションVR"
 @page.site_name = "ゲームかなー"
 @page.date = "2016/09/13"
@@ -205,7 +205,7 @@
 @page.number = 20
 @page.title = "【エロ注意】女の子のオ○ニーを覗くVR対応ゲームが公開！ｗｗｗｗｗｗ :【2ch】ニュー速クオリティ"
 @page.url = "http://news4vip.livedoor.biz/archives/52181576.html"
-@page.image = "/images//assets//assets/3054f567-s.jpg"
+@page.image = "/images/3054f567-s.jpg"
 @page.body = "1: 以下、ニュー速クオリティでお送りします 2016/09/05(月) 19:51:48.80 ID:ID:HCc5jnqE0.net   シャワーシーン覗けるVR対応ゲーム「ハニーセレクト」シチュ多すぎマジヤバイ【アダルトご注意】   9月9日の発売が迫ったイリュージョンさんの『ハニーセレクト』。ホームペー"
 @page.site_name = "【2ch】ニュー速クオリティ"
 @page.date = "2016/09/10"
@@ -215,7 +215,7 @@
 @page.number = 19
 @page.title = "AV女優「波多野結衣」さんのアダルトVR動画がエロい！出演動画一挙公開！！"
 @page.url = "http://protosketch.co/archives/adult_category/adultvrmovie"
-@page.image = "/images//assets//assets/hathata.jpg"
+@page.image = "/images/hathata.jpg"
 @page.body = "AV女優「波多野結衣」さんのアダルト（エロ）VR動画が配信開始しました。バーチャルリアリティによって得られる臨場感あふれる波多野結衣さんのエロ姿を楽しめます。そんな波多野結衣さんのアダルト（エロ）VR動画を一挙紹介致します。"
 @page.site_name = "VR通信｜ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
 @page.date = "2016/09/10"
@@ -225,7 +225,7 @@
 @page.number = 18
 @page.title = "最先端オナニーカルチャー＝アダルトVRを体感、「日本ヤバイ！！！！」｜夜オンナ"
 @page.url = "http://woman.excite.co.jp/night/r18/wn_mess/rid_wn_mess_35311/"
-@page.image = "/images//assets//assets/wn_mess_35311_1.jpg"
+@page.image = "/images/wn_mess_35311_1.jpg"
 @page.body = "VR＝ヴァーチャルリアリティ、仮想現実。専用の装置を付けると、CGなどで作りだされた架空の空間や映像が眼前に広がり、まるでその中にいるような感覚を体験できる技術..."
 @page.site_name = "夜オンナ"
 @page.date = "2016/09/10"
@@ -235,7 +235,7 @@
 @page.number = 17
 @page.title = "シャワーシーン覗けるVR対応ゲーム「ハニーセレクト」シチュ多すぎマジヤバイ【アダルトご注意】"
 @page.url = "http://ascii.jp/limit/group/ida/elem/000/001/223/1223101/"
-@page.image = "/images//assets//assets/00a_ogp.jpg"
+@page.image = "/images/00a_ogp.jpg"
 @page.body = "イリュージョンが9月9日に発売を予定するVR対応ゲームの「ハニーセレクト」。そのVR完全版を先行体験させてもらった。ちょっとこれヤバすぎなんですけど。"
 @page.site_name = "ASCII.jp"
 @page.date = "2016/09/10"
@@ -245,7 +245,7 @@
 @page.number = 16
 @page.title = "VRとセックス 性別を超えた性体験の実現-フィクションの中のVR【第10回】 | Mogura VR"
 @page.url = "http://www.moguravr.com/vr-fiction-adult/"
-@page.image = "/images//assets//assets/FotoJet-Collage.jpg"
+@page.image = "/images/FotoJet-Collage.jpg"
 @page.body = "（柾 悟郎／ハヤカワ文庫ＪＡ） 世間に新たなガジェットが登場すると、世の男性陣の間でかなりの確率で話題になるのは、それがどの位「アダルト」な分野で活用できるのか、というようなテーマだったりします。東京では8月末にアダルトVRエキスポが、10月には札幌でも「アダルトVRカーニバル」が開催されます。 VRの特性を活かしたアダルトコンテンツも試行錯誤を繰り返しながら様々なものが登場してきているようです。 アダルトはVRの普及を牽引するのか―すんくぼのWeekly VR第4回 性的欲求というのは人の根源的なものですから、そこに関心が向かうのはVRというガジェットが社会に広く浸透するうえで必然なのかもしれません。 筆者が個人的に関心があるのは、VRを活用すれば性別を超えた性体験を得ることができる、という点です。上記の記事によると現在はまだまだ男性向けのコンテンツが主流のようですが、もしリアルに性別を超えた経験を得ることが可能になれば、大げさな話かも知れませんがかなり人間の認識に影響を与えるのではないか思うのです。 そんな事を考えていると、思い出す1冊のSF小説があります。 VRで“夜遊び” 『ヴィーナス・シティ』単行本版の表紙 1992年に刊行された『ヴィーナス・シティ』はSF作家・柾悟郎による長編小説で、第14回日本SF大賞を受賞しました。ストーリーは以下の通りです。 主人公・森口咲子は情報調査会社に勤務する26歳のOLです。やる気が無い訳ではないのですが、仕事には色々不満もあり、彼女は夜な夜な「ヴィーナス・シティ」と呼ばれるヴァーチャル空間へ繰り出しては夜遊びで会社の鬱憤を晴らしています。 VENUSとは「ネットワーク利用者のための仮想現実環境（Virtual Environment for Network Users）」の略で、咲子曰く「全感覚包括型のパソコン通信」のこと。ヴァーチャル空間内で多くの人たちが好きな“自己イメージ”（アバターのようなもの）をまとい、自由に過ごしています。 ヴィーナス・シティでは老若男女どんな姿にもなれます。アクセスしている人たちの素性は明らかではありません。男が女のイメージをまとっている事もありますし、逆もまたあります。大人か子供かも定かではありません。咲子は豊満なバストと男性器を備えた中性的なイメージでシティにアクセスしています。"
 @page.site_name = "MoguraVR"
 @page.date = "2016/09/10"
@@ -255,7 +255,7 @@
 @page.number = 15
 @page.title = "『DOAX3』VR体験会が海外から批判される理由、セクハラシミュレーターに見る性風俗の価値観と非実在性 | AUTOMATON"
 @page.url = "http://jp.automaton.am/articles/newsjp/why-dead-or-alive-xtreme-3-vr-mode-is-called-a-sexual-assault-simulator/"
-@page.image = "/images//assets//assets/20160906_WhyOppaiVRfucksWest_Header.jpg"
+@page.image = "/images/20160906_WhyOppaiVRfucksWest_Header.jpg"
 @page.body = "先日、コーエーテクモゲームスは、『DEAD OR ALIVE Xtreme 3』のPlayStation VR体験会を開催した。あくまでイベントの趣旨は、仮想現実のリゾート地で水着姿の女の子を間近で眺めたり、身体のやわらかい部分に触れたりできる技術デモに過ぎないはずだったが、YouTubeに公開された体験動画が女性に対する痴漢や性的嫌がらせを連想させるとして、海外メディアを中心に批判の的になった。それに対しユーザーの反応は賛否両論。ゲームの中で架空の女性を性的玩具として陵辱しようが現実社会には無害という主張がある一方で、セクハラシミュレーターともいえる常軌を逸した遊び方は、もはや陵辱系ゲームに見られるような性癖ポルノと変わらないという意見も。一部では、ゲームが消費者に及ぼす心理的影響に関する一般的な議論にまで発展している。国内およびアジア圏でしか発売されなかったゲームをめぐる女性キャラクターの性描写から、日本と欧米における性風俗に対する価値観の違いと、性的対象の非実在性に秘められた倫理観の落とし穴を紐解いていく。    エロいからダメなのではない 『DEAD OR ALIVE Xtreme 3』は、2016年3月にコーエーテクモゲームスから、PlayStation 4およびPlayStation Vita向けに発売されたバカンスゲーム。対戦型格闘ゲーム『DEAD OR ALIVE』シリーズに登場する女性キャラクターとなって、南国の楽園でビーチバレーやビーチフラッグといったアクティビティを満喫できるほか、島のオーナー代理となって女の子の生着替えやグラビア撮影を楽しむという遊び方もある。PlayStation 4版は、過去作にはなかった「やわらかエンジン2.0」を実装したことで、なめらかなおっぱいの揺れに加えて、水着の着崩れや日焼け、濡れた人肌の自然な描写を実現。今年10月にリリース予定のPlayStation VRにも対応している。また、PlayStation Vita版は描画性能では劣るものの、タッチスクリーンの使用が特徴だ。なお、本作はシリーズで初めて日本およびアジア圏内の発売に限定されたことで、以前から国内外で物議をかもしていた。  豊富な種類の水着が魅力"
 @page.site_name = "AUTOMATON"
 @page.date = "2016/09/10"
@@ -265,7 +265,7 @@
 @page.number = 14
 @page.title = "VRの歴史が一目で分かるインフォグラフィック | Mogura VR"
 @page.url = "http://www.moguravr.com/vr-history-infographic/"
-@page.image = "/images//assets//assets/image102.jpg"
+@page.image = "/images/image102.jpg"
 @page.body = "2016年9月現在。VR市場にはハイエンド向けのOculus RiftやHTC Vive、モバイルVR分野ではサムスンのGear VRやGoogleのCardBoardなど、さまざまなVRデバイスが出回るようになりました。VR技術の歴史は、VRデバイスの歴史、と言い換えてもいいかもしれません。 本記事では、1930年代から現在に至るまでのVR技術の歴史を、インフォグラフィックで見ていこうと思います。インフォグラフィックは、アメリカのシラキュース大学が開設している修士課程の通信制プログラムで制作されました。 以下のインフォグラフィックには、登場する技術を三つに分類しています。 １．前身技術（物理世界とバーチャルな世界にまたがっているもの） ２．AR（ユーザーの現実世界にバーチャルな要素を付与するもの） ３．VR（ユーザーをバーチャルな世界に没入させるもの） 1930年代   「Link Trainer」は1930年代にリンク氏によって開発された飛行機シミュレーター。前身技術として紹介されています。 最初はアミューズメントパークなどに遊具として導入されました。ところがエアメール便の配達事故や第二次世界大戦を経て軍などが、徐々に飛行士訓練のために「Link Trainer」を導入し始めました。 1940年代   1940年代に徐々に広まったのは「Sawyer's」社による「View-Master」です。「リール」を装填して除くことで、両眼立体視を利用してカラーの3D画像を見ることができるというもの。 ひとつのリールで7つまで画像を見れたことが当時特徴的とされ、推定ではビューワー1億台、リールは15億本売れたとされています。もとは大人向けの教育ツールとして開発されましたが、徐々に子供のおもちゃへとシフトしていきました。 1950年代   1957年に登場した「Sensorama」は、Morton Heiligが開発した筐体型のマシーンです。プレイヤーはイスに座ってマシーンをのぞき込みます。正面には比較的大きなスクリーンがあり、3D映像を楽しむことが可能。加えて、アロマなどの香りを生成、イスが振動、送風などの機能も搭載されていました。インフォグラフィックではARに分類されています。 1960年代"
 @page.site_name = "MoguraVR"
 @page.date = "2016/09/10"
@@ -275,7 +275,7 @@
 @page.number = 13
 @page.title = "任天堂の宮本茂氏、VRに否定的「我々は家族で一緒にプレイできるゲームを作りたい」：はちま起稿"
 @page.url = "http://blog.esuteru.com/archives/20001874.html"
-@page.image = "/images//assets//assets/6e9925b29a929bb426b0e0304fed6581.jpg"
+@page.image = "/images/6e9925b29a929bb426b0e0304fed6581.jpg"
 @page.body = "任天堂の宮本茂氏、VRに否定的「我々は家族で一緒にプレイできるゲームを作りたい」の記事詳細。国内最大のエンタメまとめメディアサイト。ゲーム情報、アニメや漫画、時事ネタなど、たくさんのネタを面白おかしく紹介します。"
 @page.site_name = "はちま起稿"
 @page.date = "2016/09/10"
@@ -285,7 +285,7 @@
 @page.number = 12
 @page.title = "【美少女】今後このレベルがVRに来るらしいｗｗｗｗｗｗ : Y速報"
 @page.url = "http://ysokuhou.blog.jp/archives/65727169.html"
-@page.image = "/images//assets//assets/59679b13.jpg"
+@page.image = "/images/59679b13.jpg"
 @page.body = "       2015年10月にTwitterで公開され「実写に見える」と話題になった3DCG美少女「Saya」（関連記事）の、  2016年版が公開されました。さらにリアルになり、なんだかどこかでお会いしたことがあるような気さえします。    　作成したのは、夫婦でCGを作成されている@moj"
 @page.site_name = "Y速報"
 @page.date = "2016/09/10"
@@ -295,7 +295,7 @@
 @page.number = 11
 @page.title = "【画像あり】女の子のシャワーを覗くVR対応ゲームが公開！シチュ多すぎマジヤバイ : キニ速"
 @page.url = "http://blog.livedoor.jp/kinisoku/archives/4679446.html"
-@page.image = "/images//assets//assets/4f78fa2b.jpg"
+@page.image = "/images/4f78fa2b.jpg"
 @page.body = "1： ダブルニードロップ(千葉県)＠＼(^o^)／：2016/09/05(月) 19:51:48.80ID:HCc5jnqE0.netシャワーシーン覗けるVR対応ゲーム「ハニーセレクト」シチュ多すぎマジヤバイ【アダルトご注意】    9月9日の発売が迫ったイリュージョンさんの『ハニーセレクト』。ホームペー"
 @page.site_name = "キニ速"
 @page.date = "2016/09/10"
@@ -305,7 +305,7 @@
 @page.number = 10
 @page.title = "AAAと「黒ひげ危機一発」　スマホでVR体験"
 @page.url = "http://www.oricon.co.jp/news/2078152/full/"
-@page.image = "/images//assets//assets/2078152_201609090748887001473390022c.jpg"
+@page.image = "/images/2078152_201609090748887001473390022c.jpg"
 @page.body = "ニュース｜　スマートフォンにダウンロード（無料）するだけで360度映像が楽しめるVR視聴専用アプリ「dTVVR」に、男女7人組ダンスボーカルグループ・AAAのオリジナルVR（バーチャルリアリティー）作品第2弾『AAA7番勝負-前半戦-／後半戦-』が登場。9日より配信開始される。"
 @page.site_name = "ORICON STYLE"
 @page.date = "2016/09/10"
@@ -315,7 +315,7 @@
 @page.number = 9
 @page.title = "任天堂・宮本茂さん「マリオとVRは相性が良くない。プレイヤーが現実から閉鎖されるVRでは家族と一緒に遊べない」 : ゲームかなー"
 @page.url = "http://blog.gamekana.com/archives/8682123.html"
-@page.image = "/images//assets//assets/8fcb2db2.png"
+@page.image = "/images/8fcb2db2.png"
 @page.body = "宮本茂さん"
 @page.site_name = "ゲームかなー"
 @page.date = "2016/09/10"
@@ -325,7 +325,7 @@
 @page.number = 8
 @page.title = "『お姉チャンバラ』シリーズがPS4でガンアクションに!?　そしてプレステVR向けに新作ギャルゲーも登場!! - ライブドアニュース"
 @page.url = "http://news.livedoor.com/article/detail/11994454/"
-@page.image = "/images//assets//assets/94f5e_1390_bbe69719_49ebf283.jpg"
+@page.image = "/images/94f5e_1390_bbe69719_49ebf283.jpg"
 @page.body = "株式会社ソニー・インタラクティブエンタテインメントが「PlayStation Meeting 2016」にて、「PlayStation 4 Pro」(以下、PS4 Pro)および「新型PS4」を発表。大きな注目を集める中、株式会社ディースリー・パブリ"
 @page.site_name = "ライブドアニュース"
 @page.date = "2016/09/10"
@@ -335,7 +335,7 @@
 @page.number = 7
 @page.title = "『実況パワフルプロ野球 2016』前田健太や石川柊太などが追加される無料アプデver.1.05配信開始- 記事詳細｜Infoseekニュース"
 @page.url = "http://news.infoseek.co.jp/article/inside_101727/"
-@page.image = "/images//assets//assets/infoseeknewslogo.jpg"
+@page.image = "/images/infoseeknewslogo.jpg"
 @page.body = "コナミデジタルエンタテインメントは、PS4/PS3/PS Vita『実況パワフルプロ野球 2016』において無料ア"
 @page.site_name = "Infoseekニュース"
 @page.date = "2016/09/10"
@@ -345,7 +345,7 @@
 @page.number = 6
 @page.title = "動画ダウンロードやビデオ再生、ストリーミングに関する豆知識やソフトの情報をすべて網羅しようとするブログ"
 @page.url = "http://ting456789.n-da.jp/"
-@page.image = "/images//assets//assets/profile.jpg"
+@page.image = "/images/profile.jpg"
 @page.body = "みなさんと一緒に、最新のIT情報をシェアする。また、最新の製品情報も公開する。例えば、動画再生ソフトとか、DVDリッピングソフトなどだ。もし何かも違ったところがあれば、教えてください。"
 @page.site_name = "動画ダウンロードやビデオ再生、ストリーミングに関する豆知識やソフトの情報をすべて網羅しようとするブログ"
 @page.date = "2016/09/10"
@@ -355,7 +355,7 @@
 @page.number = 5
 @page.title = "ブックパス::カテゴリ一覧"
 @page.url = "https://www.bookpass.auone.jp/category"
-@page.image = "/images//assets//assets/bookpass.jpg"
+@page.image = "/images/bookpass.jpg"
 @page.body = "「ブックパスで取り扱っています::カテゴリ一覧│」"
 @page.site_name = "ブックパス"
 @page.date = "2016/09/10"
@@ -365,7 +365,7 @@
 @page.number = 4
 @page.title = "エロゲに関連する732件のまとめ"
 @page.url = "http://togetter.com/t/%E3%82%A8%E3%83%AD%E3%82%B2"
-@page.image = "/images//assets//assets/tg_ogp_default4.png"
+@page.image = "/images/tg_ogp_default4.png"
 @page.body = "タグ「エロゲ」に関連する732件のまとめをお届けします。"
 @page.site_name = "Togetter"
 @page.date = "2016/09/10"
@@ -375,7 +375,7 @@
 @page.number = 3
 @page.title = "みうらじゅん＆前野健太が歌唱、安齋肇監督の青春ポルノ『変態だ』ED曲"
 @page.url = "http://www.cinra.net/news/20160909-hentaida"
-@page.image = "/images//assets//assets/20160909-hentaida.jpg"
+@page.image = "/images/20160909-hentaida.jpg"
 @page.body = "映画『変態だ』のエンディングテーマが、みうらじゅんと前野健太が歌う楽曲“Kill Bear”になることが発表された。12月10日から東京・新宿ピカデリ…"
 @page.site_name = "CINRA.NET"
 @page.date = "2016/09/10"
@@ -385,7 +385,7 @@
 @page.number = 2
 @page.title = "DMM、18歳未満出演の「イメージビデオ」取り扱い停止"
 @page.url = "http://www.itmedia.co.jp/news/articles/1609/09/news086.html"
-@page.image = "/images//assets//assets/l_yx_dmm.jpg"
+@page.image = "/images/l_yx_dmm.jpg"
 @page.body = "「児童ポルノと疑われる動画を配信している」と人権団体から名指しで指摘を受け、DMMは18歳未満が出演する「イメージビデオ」の配信を停止したと発表した。"
 @page.site_name = "ITmedia ニュース"
 @page.date = "2016/09/10"
@@ -395,7 +395,7 @@
 @page.number = 1
 @page.title = "ホーム"
 @page.url = "http://www.subete360.com/"
-@page.image = "/images//assets//assets/meta.png"
+@page.image = "/images/meta.png"
 @page.body = "SUBETE360はVR/ARの総合情報サイトです。国内・海外の最新ニュースや動画、 VR/ARコンテンツの全てをお届けします！SUBETE360という名は「 VR/ARの全てにアンテナを張ってナビゲートしたい」という気持ちが込められています。"
 @page.site_name = "SUBETE360"
 @page.date = "2016/09/10"
