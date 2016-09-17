74d1aa7f7c616b309bf8a03990c8020b8e4c772a
Hashimoto <hashimoto@dl.kuis.kyoto-u.ac.jp>
2016-09-18 01:13:56 +0900

    update20160918

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,101 +1,103 @@
 #coding: utf-8
 @page = Page.new
-@page.number = 50
-@page.title = "東京ゲームショウ、VRで揉み放題＆美少女と一緒に入浴体験とやりたい放題 : ２ちゃんねるのガイドライン"
-@page.url = "http://guideline-2ch.com/archives/10705979.html"
-@page.image = "/images/9b55ed12-s.jpg"
-@page.body = "1: 風吹けば名無し＠無断転載禁止  2016/09/15(木) 12:28:12.21 ID:mktWmI7ha      転載元http://raptor.2ch.net/test/read.cgi/livejupiter/1473910092/ "
-@page.site_name = "２ちゃんねるのガイドライン"
-@page.date = "2016/09/17"
+@page.number = 60
+@page.title = "本誌デスクがアダルトVRを体験　「褐色の巨乳ダンサーが…」 〈AERA〉｜dot.ドット"
+@page.url = "https://dot.asahi.com/aera/2016091500260.html"
+@page.image = "/images/2016091500260_1.jpg"
+@page.body = "　大きなゴーグルをかけて絶叫する人。惚（ほう）けた表情でベッドに横たわる人……。8月27日、都内某所で開催された第2回「アダルトVRエキスポ」は、その名に恥じない異様な空気に包まれたイベントだった。「..."
+@page.site_name = "dot.〈AERA〉"
+@page.date = "2016/09/18"
 @page.save
 
 @page = Page.new
-@page.number = 49
-@page.title = "韓国人「日本人にVRを使わせるとこうなる…VRゲームの悲しい現実」 : カイカイ反応通信"
-@page.url = "http://blog.livedoor.jp/kaikaihanno/archives/49546337.html"
-@page.image = "/images/5b12b6b6.jpg"
-@page.body = "韓国のネット掲示板に「VRゲームの悲しい現実」というスレッドが立っていたのでご紹介。"
-@page.site_name = "カイカイ反応通信"
-@page.date = "2016/09/17"
-@page.save
-
-@page = Page.new
-@page.number = 48
-@page.title = "【TGS】VR空間の美女集団から1人を選べる？ 脳波は僕を裏切らない！"
-@page.url = "http://kai-you.net/article/33257"
-@page.image = "/images/IMG_00055.jpg"
-@page.body = "9月15日（木）から9月18（日）にかけて、千葉・幕張メッセで行われている日本最大級のゲームの祭典「東京ゲームショウ2016」（TGS2016）。国際展示場9ホール・9-W25の電通サイエンスジャムブースでは、VR映像コンテンツ「脳波LOVEチェッカー」が展示されています。..."
+@page.number = 59
+@page.title = "【動画】脳波LOVEチェッカーでVR美女が脳波を刺激！@TGS2016"
+@page.url = "http://kai-you.net/article/33287"
+@page.image = "/images/ARTICLE_NOUHA_tgs_bio_thum_201608_02.jpg"
+@page.body = "9月15日（木）から18日（日）までの4日間、幕張メッセにて「東京ゲームショウ2016」が開催中だ。KAI-YOU編集部では、慶應義塾大学准教授・満倉靖恵さんの研究「感性把握アルゴリズム」を用いたVRコンテンツ「脳波LOVEチェッカー」を体験することに。「脳波LOVEチェ..."
 @page.site_name = "KAI-YOU.net | POP is Here ."
-@page.date = "2016/09/17"
+@page.date = "2016/09/18"
 @page.save
 
 @page = Page.new
-@page.number = 47
-@page.title = "【悲報】東京ゲームショウで胸を揉みまくれるVRゲーム、揉みしだかれすぎて故障する : オレ的ゲーム速報＠刃"
-@page.url = "http://jin115.com/archives/52147229.html"
-@page.image = "/images/cb2590b9.jpg"
-@page.body = "【悲報】東京ゲームショウで胸を揉みまくれるVRゲーム、揉みしだかれすぎて故障するの記事ページ"
-@page.site_name = "オレ的ゲーム速報@刃"
-@page.date = "2016/09/17"
+@page.number = 58
+@page.title = "VRはゲームで多様化している：東京ゲームショウ2016ギャラリー- 記事詳細｜Infoseekニュース"
+@page.url = "http://news.infoseek.co.jp/article/wired_240587/"
+@page.image = "/images/infoseeknewslogo.jpg"
+@page.body = "9月15日（木）から18日（日）まで開催されている日本最大のゲームイヴェント、「東京ゲームショウ2016」（以下、"
+@page.site_name = "Infoseekニュース"
+@page.date = "2016/09/18"
 @page.save
 
 @page = Page.new
-@page.number = 46
-@page.title = "VRモード搭載の美少女RPG『オルタナティブガールズ』CM放送キャンペーンでバニーガールもらえる！ [ファミ通App]"
-@page.url = "http://app.famitsu.com/20160914_834416/"
-@page.image = "/images/69eb07843312ec26e186fb6229f3d789-300x300.jpg"
-@page.body = "サイバーエージェントが配信するVRモード搭載の美少女スマホアプリRPG『オルタナティブガールズ』初のテレビCMが2016年9月15日より放送開始する。"
-@page.site_name = "ファミ通App"
-@page.date = "2016/09/17"
+@page.number = 57
+@page.title = "本誌デスクがアダルトVRを体験　「褐色の巨乳ダンサーが…」〈AERA〉（dot.） - Yahoo!ニュース"
+@page.url = "http://zasshi.news.yahoo.co.jp/article?a=20160915-00000260-sasahi-sci&p=2"
+@page.image = "/images/20160915-00000260-sasahi-000-view.jpg"
+@page.body = "大きなゴーグルをかけて絶叫する人。惚（ほう）けた表情でベッドに横たわる人……。8月2 - Yahoo!ニュース(dot.)"
+@page.site_name = "Yahoo!ニュース"
+@page.date = "2016/09/18"
 @page.save
 
 @page = Page.new
-@page.number = 45
-@page.title = "VR美少女RPG『オルタナティブガールズ』初のテレビCMオンエア。伊藤美来さん、上坂すみれさんたちのサイン入りCDがもらえる..."
-@page.url = "http://www.gpara.com/infos/view/37486"
-@page.image = "/images/thumbnail.jpg"
-@page.body = "　サイバーエージェントは、iOS/Android端末向けスマホゲーム『オルタナティブガールズ』において、初のテレビCMを2016年9月15日（木）より、全国での放送を開始します。　『オルタナティ..."
-@page.site_name = "ジーパラドットコム｜VR美少女RPG『オルタナティブガールズ』初のテレビCMオンエア。伊藤美来さん、上坂すみれさんたちのサイン入りCDがもらえるキャンペーンも"
-@page.date = "2016/09/17"
+@page.number = 56
+@page.title = "VR空間の美女集団から1人を選べる？ 脳波は僕を裏切らない！（KAI-YOU.net） - グノシー"
+@page.url = "https://gunosy.com/articles/a6Tjd"
+@page.image = "/images/6149bd3527fd7bc4123cbc9026145ac3_content.jpg"
+@page.body = "9月15日（木）から9月18（日）にかけて、千葉・幕張メッセで行われている日本最大級のゲームの祭典「東京ゲームショウ2016」（TGS2016）。
+国際展示場9ホール・9-W25の電通サイエンスジャムブースでは、VR映像コンテンツ「脳波LOVEチェッカー」が展示されています。
+これは、慶應義塾大学准教授・満倉靖恵さんの研究である人間の感情を数値化できる「感性把握アルゴリズム」を応用したコンテンツ。"
+@page.site_name = "グノシー 雑談力がつくニュースアプリ"
+@page.date = "2016/09/18"
 @page.save
 
 @page = Page.new
-@page.number = 44
-@page.title = "TGS2016で美少女武将たちとの混浴をVRで楽しめる！"
-@page.url = "http://dengekionline.com/elem/000/001/366/1366063/"
-@page.image = "/images/poem_07_cs1w1_1000x525.jpg"
-@page.body = "『パズルオブエンパイア（パズエン）』が“東京ゲームショウ2016”に出展。出展ブースではゲーム内キャラクターと一緒にお風呂に入るVR体験を楽しめます。"
+@page.number = 55
+@page.title = "海外「VRデモで二次元美少女の胸を揉み過ぎた結果ｗｗｗｗｗｗｗｗｗ」"
+@page.url = "http://world-action.net/archives/49119"
+@page.image = "/images/too_much_fondling.png"
+@page.body = "詳しくは投稿をご覧ください。"
+@page.site_name = "World Action"
+@page.date = "2016/09/18"
+@page.save
+
+@page = Page.new
+@page.number = 54
+@page.title = "VR元年宣言の東京ゲームショウでプレステVRにチャレンジ!?- 記事詳細｜Infoseekニュース"
+@page.url = "http://news.infoseek.co.jp/article/walkerplus_87234/"
+@page.image = "/images/infoseeknewslogo.jpg"
+@page.body = "9月17日(土)・18日(日)に開催される日本最大のゲームの祭典「東京ゲームショウ2016」(9月15日・16日の"
+@page.site_name = "Infoseekニュース"
+@page.date = "2016/09/18"
+@page.save
+
+@page = Page.new
+@page.number = 53
+@page.title = "本誌デスクがアダルトVRを体験　「褐色の巨乳ダンサーが…」 - (3/3)｜ニフティニュース"
+@page.url = "https://news.nifty.com/article/magazine/12106-20160917-2016091500260/3"
+@page.image = "/images/first-12106-20160917-2016091500260-magazine.jpg"
+@page.body = "　大きなゴーグルをかけて絶叫する人。惚（ほう）けた表情でベッドに横たわる人……。8月27日、都内某所で開催された第2回「アダルトVRエキスポ」は、その名に恥じない異様な空気に包まれたイベントだった。「…"
+@page.site_name = "ニフティニュース"
+@page.date = "2016/09/18"
+@page.save
+
+@page = Page.new
+@page.number = 52
+@page.title = "美少女キャラと“保健室VR”【TGS2016】"
+@page.url = "http://trendy.nikkeibp.co.jp/atcl/pickup/15/1003590/091600526/"
+@page.image = "/images/top_m.jpg"
+@page.body = "Live2Dのブースでは、2Dのイラストから360度動くキャラクターモデルを製作する「Live2D Euclid」のデモを行っている。HTC ViveでVRコンテンツとして体験できる。"
+@page.site_name = "日経トレンディネット"
+@page.date = "2016/09/18"
+@page.save
+
+@page = Page.new
+@page.number = 51
+@page.title = "東京ゲームショウでスキーや美女との混浴を体験！ オススメVRブースを紹介【TGS2016】"
+@page.url = "http://dengekionline.com/elem/000/001/368/1368845/"
+@page.image = "/images/tgs_07_cs1w1_450x253.jpg"
+@page.body = "“東京ゲームショウ2016（TGS2016）”にはVR体験ができるブースが多数登場。そのなかでもオススメのブースを紹介します！"
 @page.site_name = "電撃オンライン"
-@page.date = "2016/09/17"
-@page.save
-
-@page = Page.new
-@page.number = 43
-@page.title = "家電会議 - 美女との距離が半端なく近くて仮想現実なのに照れてしまうPS VR向けゲーム「サマーレッスン：宮本ひかり セブンデイズルーム」を体験 - GIGAZINE"
-@page.url = "https://kadenkaigi.com/entry/301445884"
-@page.image = "/images/301445884-1473944335_l.jpg"
-@page.body = "PlayStation VRと同時にリリースされるゲーム「 サマーレッスン：宮本ひかり セブンデイズルーム 」が、 東京ゲームショウ2016 のプレイステーショ..."
-@page.site_name = "家電会議"
-@page.date = "2016/09/17"
-@page.save
-
-@page = Page.new
-@page.number = 42
-@page.title = "美少女と混浴でキャッキャウフフできる最強のVRが東京ゲームショウで爆誕【TGS 2016】 [ファミ通App]"
-@page.url = "http://app.famitsu.com/20160915_836131/"
-@page.image = "/images/aikon-3-300x300.jpg"
-@page.body = "幕張メッセで開催中の東京ゲームショウ2016。美少女キャラとVRで混浴を楽しめる芸者東京エンターテインメントブースでひとっ風呂浴びてきた。"
-@page.site_name = "ファミ通App"
-@page.date = "2016/09/17"
-@page.save
-
-@page = Page.new
-@page.number = 41
-@page.title = "AV男優、アダルトVRフェスの主催者という噂を闇の業界人に流されたお話 - KAI-YOU BLOG"
-@page.url = "http://kai-you.hatenablog.com/entry/2016/09/14/213058"
-@page.image = "/images/20160914162127.jpg"
-@page.body = "※このブログはKAI-YOU mini blogに掲載予定でしたが長文のため単独掲載となりました こんばんは、よしだです。 先日、社内で連絡ツールとして使用しているSlackにこんなメッセージが投下されました。 これは、代表の米村と編集長の新見の間で交わされた会話です。 ちげーよおおおおおお！！！！！！！！！ とまぁ、ちょっと興奮してしまいましたが、書いてある通り、なんと某応援アプリで知られる会社さんの中で、僕があの「アダルトVRフェスタ」の主催者であり、その理由は僕がAV男優だから、という噂が流れているそうなのです＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾＾その会社の人たちも別の会社の人から聞い…"
-@page.site_name = "KAI-YOU BLOG"
-@page.date = "2016/09/17"
+@page.date = "2016/09/18"
 @page.save
 
