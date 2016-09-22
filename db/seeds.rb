0408f6a1ed72638b6439438b840acee6041e76e5
Hashimoto <hashimoto@dl.kuis.kyoto-u.ac.jp>
2016-09-22 20:21:26 +0900

    update20160922

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,61 @@
 #coding: utf-8
 @page = Page.new
-@page.number = 72
-@page.title = "美女に囲まれる幸せ TGS2016のコンパニオンを360度カメラで撮影してきた | Mogura VR"
-@page.url = "http://www.moguravr.com/tgs2016-360-vr/"
-@page.image = "/images/image00149.jpg"
-@page.body = "最近、360度全天周の撮影は、ますます便利に、簡単になってきています。 枠に切り取るのでなく、世界そのものを丸ごとおさめてしまう360度撮影。「どのように撮ると良いのか」、という知見はまだまだ少なく、ポーズや立ち位置など、写真の撮り方から考える楽しさもあります。 Mogura VRではこれまで、各種イベントやハロウィンなど、舞台を変えて全天周写真を撮影する企画を行ってきました。 （関連記事） 360度カメラでどう撮る？冬の雪山でRICOH THETA S を使った全天球撮影に挑戦 360度カメラでどう撮る？東京ゲームショウでコンパニオンの皆さんに全天周で自撮り撮影をしてもらった 渋谷ハロウィン2015の仮装を360度カメラ『RICOH THETA S』で自撮り撮影してもらった 今回の舞台は、9月15日から18日まで幕張メッセにて開催中の「東京ゲームショウ 2016」。新作ゲームからVRまで、魅力的なコンテンツや大規模展示に溢れています。しかしゲームだけでなく、会場を彩るコンパニオンの皆さんも、毎年会場に華を添えて賑わせています。 今回は昨年のTGSに引き続き、各ブースのコンパニオンの皆さんに、全天球カメラを使った自分撮りをしていただきました。カメラマンが写ってしまうため、一脚の先端にカメラを装着し、自身で持ってもらった撮影を行ったものになります。 ※なお本記事中の360度写真は見渡すことができます。PCで見る際は、写真をドラッグすることで、スマートフォンで見る際は写真に指を滑らせることで、見回すことができます。 ※Google CardboardなどのスマホVRデバイスを持っている人は、写真をタップすると自動的にVRモードで起動し、より没入感のある体験が可能です。 ※スマートニュースなどの外部配信でご覧になっている方は、360度写真を見ることができませんので、こちらよりGoogle Chrome等のブラウザでご覧ください  １ 最初にコナミブースでは、四人のコンパニオンさんに、360度写真王道ともいえる円陣をお願いしました。    ２ 完美世界ブースのコスプレコンパニオンさん。装備を構えてポーズを取ってもらいました。    ３ Xperiaブースでは初音ミクの立て看板と三人仲良く撮影！あえてカメラ目線を外して、ミクさんの方を見つめてもらいました。    ４"
-@page.site_name = "MoguraVR"
-@page.date = "2016/09/20"
+@page.number = 78
+@page.title = "絶対に売れるエロVR思いついたわｗｗｗｗｗｗｗ : ニコニコVIP2ch"
+@page.url = "http://blog.livedoor.jp/nicovip2ch/archives/1968295.html"
+@page.image = "/images/61e3a713-s.jpg"
+@page.body = "1:   2016/09/20(火) 01:22:20.592 ID:HRxvOP4x0 好きな街中の映像を選択できて  その中で野外オナニーが楽しめるというものだ  通行人はこっちを見てきて色んな反応をしてくれる  気持ち悪がったり、通報したり、写真撮ったり  色んな人々が様々行動を起こす中でオナニーが"
+@page.site_name = "ニコニコVIP2ch"
+@page.date = "2016/09/22"
 @page.save
 
 @page = Page.new
-@page.number = 71
-@page.title = "本誌デスクがアダルトVRを体験　「褐色の巨乳ダンサーが…」 〈AERA〉｜dot.ドット"
-@page.url = "https://dot.asahi.com/aera/2016091500260.html?page=2"
-@page.image = "/images/2016091500260_1.jpg"
-@page.body = "　大きなゴーグルをかけて絶叫する人。惚（ほう）けた表情でベッドに横たわる人……。8月27日、都内某所で開催された第2回「アダルトVRエキスポ」は、その名に恥じない異様な空気に包まれたイベントだった。「..."
-@page.site_name = "dot.〈AERA〉"
-@page.date = "2016/09/20"
+@page.number = 77
+@page.title = "【悲報】東京ゲームショウ、VRで美少女のおっぱい揉んだり一緒にお風呂に入ったりとやりたい放題 : マジキチ速報"
+@page.url = "http://majikichi.com/archives/9358879.html"
+@page.image = "/images/66f9e250-s.jpg"
+@page.body = "【悲報】東京ゲームショウ、VRでおっぱい揉み放題＆美少女と一緒に入浴体験とやりたい放題1：風吹けば名無し＠＼(^o^)／2016/09/15(木) 12:28:12.21 ID:mktWmI7ha.net        引用元http://tomcat.2ch.sc/test/read.cgi/livejupiter/1473910092"
+@page.site_name = "マジキチ速報"
+@page.date = "2016/09/22"
+@page.save
+
+@page = Page.new
+@page.number = 76
+@page.title = "注目度はゲームショー以上!?バーチャルエロの最先端「アダルトVRエキスポ2016」潜入レポ（＠ＤＩＭＥ） - Yahoo!ニュース"
+@page.url = "http://zasshi.news.yahoo.co.jp/article?a=20160922-00010007-dime-sci"
+@page.image = "/images/20160922-00010007-dime-000-view.jpg"
+@page.body = "先日行われた「アダルトVRエキスポ2016」の模様をお届けしたいと思う。筆者の友人であ - Yahoo!ニュース(＠ＤＩＭＥ)"
+@page.site_name = "Yahoo!ニュース"
+@page.date = "2016/09/22"
+@page.save
+
+@page = Page.new
+@page.number = 75
+@page.title = "【東京ゲームショウ】二次元美少女の胸を揉むVRデモが壊れる : VR速報"
+@page.url = "http://vr2ch.me/archives/6639253.html"
+@page.image = "/images/40a4bd83-s.jpg"
+@page.body = "1: 名無しさん必死だな＠＼(^o^)／  2016/09/17(土) 19:55:30.66 ID:yOVAHIiVr 二次元美少女の胸を揉めるVRデモが「東京ゲームショウ2016」に出展され、大きな話題になっています。  このデモはゲームメーカー・エムツーの作品で、同社が手がけるキャラクターアニメーション"
+@page.site_name = "VR速報"
+@page.date = "2016/09/22"
+@page.save
+
+@page = Page.new
+@page.number = 74
+@page.title = "【朗報】VRのエロゲがヤバすぎるｗｗｗｗｗ : 暇人＼(^o^)／速報"
+@page.url = "http://himasoku.com/archives/51970885.html"
+@page.image = "/images/e0611cfc.png"
+@page.body = "  1: 風吹けば名無し＠＼(^o^)／  2016/09/21(水) 16:44:04.59 ID:rQTRCE/AM.net　 HTC Vive+カスタムメイド3D2を手にしたワイ pic.twitter.com/4jaC4k8erA— 水田ケンジ＠猛虎熊野と鈴谷さん委託中 (@suidengetsu) 2016年9月20日https://twitter.com/suidengetsu/sta"
+@page.site_name = "暇人＼(^o^)／速報"
+@page.date = "2016/09/22"
+@page.save
+
+@page = Page.new
+@page.number = 73
+@page.title = "【朗報】VRのエロゲがヤバすぎるｗｗｗｗ : キニ速"
+@page.url = "http://blog.livedoor.jp/kinisoku/archives/4686651.html"
+@page.image = "/images/06361d3c.jpg"
+@page.body = "1：風吹けば名無し＠＼(^o^)／：2016/09/21(水) 16:44:04.59ID:rQTRCE/AM.net水田ケンジ＠猛虎熊野と鈴谷さん委託中@suidengetsuHTC Vive+カスタムメイド3D2を手にしたワイ https://t.co/4jaC4k8erA2016/09/20 23:13:24https://video.twimg.com/ext_tw_video/77823447320475"
+@page.site_name = "キニ速"
+@page.date = "2016/09/22"
 @page.save
 
