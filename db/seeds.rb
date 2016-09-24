5cf9a2e624fffc2a7b497f2af586fdb580b57472
Hashimoto <hashimoto@dl.kuis.kyoto-u.ac.jp>
2016-09-24 16:09:10 +0900

    update20160924

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,61 +1,91 @@
 #coding: utf-8
 @page = Page.new
-@page.number = 78
-@page.title = "絶対に売れるエロVR思いついたわｗｗｗｗｗｗｗ : ニコニコVIP2ch"
-@page.url = "http://blog.livedoor.jp/nicovip2ch/archives/1968295.html"
-@page.image = "/images/61e3a713-s.jpg"
-@page.body = "1:   2016/09/20(火) 01:22:20.592 ID:HRxvOP4x0 好きな街中の映像を選択できて  その中で野外オナニーが楽しめるというものだ  通行人はこっちを見てきて色んな反応をしてくれる  気持ち悪がったり、通報したり、写真撮ったり  色んな人々が様々行動を起こす中でオナニーが"
+@page.number = 87
+@page.title = "【悲報】東京ゲームショウ、VRでおっぱい揉み放題＆美少女と一緒に入浴体験とやりたい放題ｗｗｗｗｗｗｗｗ : ニコニコVIP2ch"
+@page.url = "http://blog.livedoor.jp/nicovip2ch/archives/1967904.html"
+@page.image = "/images/14c989f1-s.jpg"
+@page.body = "1:   2016/09/15(木) 12:56:57.750 ID:PuJXFaZaa 引用元: ・【悲報】東京ゲームショウ、VRでおっぱい揉み放題＆美少女と一緒に入浴体験とやりたい放題"
 @page.site_name = "ニコニコVIP2ch"
-@page.date = "2016/09/22"
+@page.date = "2016/09/24"
 @page.save
 
 @page = Page.new
-@page.number = 77
-@page.title = "【悲報】東京ゲームショウ、VRで美少女のおっぱい揉んだり一緒にお風呂に入ったりとやりたい放題 : マジキチ速報"
-@page.url = "http://majikichi.com/archives/9358879.html"
-@page.image = "/images/66f9e250-s.jpg"
-@page.body = "【悲報】東京ゲームショウ、VRでおっぱい揉み放題＆美少女と一緒に入浴体験とやりたい放題1：風吹けば名無し＠＼(^o^)／2016/09/15(木) 12:28:12.21 ID:mktWmI7ha.net        引用元http://tomcat.2ch.sc/test/read.cgi/livejupiter/1473910092"
-@page.site_name = "マジキチ速報"
-@page.date = "2016/09/22"
+@page.number = 86
+@page.title = "【悲報】東京ゲームショウ、VRでおっぱい揉み放題＆美少女と一緒に入浴体験とやりたい放題 : ニコニコVIP2ch"
+@page.url = "http://blog.livedoor.jp/nicovip2ch/archives/1967962.html"
+@page.image = "/images/69505473-s.jpg"
+@page.body = "1:   2016/09/15(木) 12:56:57.750 ID:PuJXFaZaa 引用元: ・【悲報】東京ゲームショウ、VRでおっぱい揉み放題＆美少女と一緒に入浴体験とやりたい放題"
+@page.site_name = "ニコニコVIP2ch"
+@page.date = "2016/09/24"
 @page.save
 
 @page = Page.new
-@page.number = 76
-@page.title = "注目度はゲームショー以上!?バーチャルエロの最先端「アダルトVRエキスポ2016」潜入レポ（＠ＤＩＭＥ） - Yahoo!ニュース"
-@page.url = "http://zasshi.news.yahoo.co.jp/article?a=20160922-00010007-dime-sci"
-@page.image = "/images/20160922-00010007-dime-000-view.jpg"
-@page.body = "先日行われた「アダルトVRエキスポ2016」の模様をお届けしたいと思う。筆者の友人であ - Yahoo!ニュース(＠ＤＩＭＥ)"
-@page.site_name = "Yahoo!ニュース"
-@page.date = "2016/09/22"
+@page.number = 85
+@page.title = "【画像】女の子が、VR空間でおっぱいを揉まれまくった結果ｗｗｗｗｗｗ : ニコニコVIP2ch"
+@page.url = "http://blog.livedoor.jp/nicovip2ch/archives/1967880.html"
+@page.image = "/images/70243be0-s.jpg"
+@page.body = " 引用元: ・【悲報】VR空間でおっぱいを揉まれまくった女の子が壊れるｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗ "
+@page.site_name = "ニコニコVIP2ch"
+@page.date = "2016/09/24"
 @page.save
 
 @page = Page.new
-@page.number = 75
-@page.title = "【東京ゲームショウ】二次元美少女の胸を揉むVRデモが壊れる : VR速報"
-@page.url = "http://vr2ch.me/archives/6639253.html"
-@page.image = "/images/40a4bd83-s.jpg"
-@page.body = "1: 名無しさん必死だな＠＼(^o^)／  2016/09/17(土) 19:55:30.66 ID:yOVAHIiVr 二次元美少女の胸を揉めるVRデモが「東京ゲームショウ2016」に出展され、大きな話題になっています。  このデモはゲームメーカー・エムツーの作品で、同社が手がけるキャラクターアニメーション"
-@page.site_name = "VR速報"
-@page.date = "2016/09/22"
+@page.number = 84
+@page.title = "【朗報】VRでエロゲする男の姿が高次元過ぎる！！ | アニメ時代喋り場"
+@page.url = "http://anime-talk.biz/archives/2315"
+@page.image = "/images/25e45660.jpg"
+@page.body = "風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:44:04.59 ID:rQTRCE/AM.net https://twitter.com/suidengetsu/status/778235617087938560  HTC Vive+カスタムメイド3D2を手にしたワイ pic.twitter.com/4jaC4k8erA — 水田ケンジ＠猛虎熊野と鈴谷さん委託中 (@suidengetsu) 2016年9月20日      風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:44:27.53 ID:rQTRCE/AM.net これはヤバイwwwww     風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:44:48.76 ID:rQTRCE/AM.net これするのにいくら用意すればええんや？     風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:45:29.03 ID:rQTRCE/AM.net これもう女いらんやろ...     風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:45:44.40 ID:+FxRHb4s0.net まだまだ黎明期よ     風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:45:50.68 ID:5H70hRQl0.net 死ねばいいのに     風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:46:50.59 ID:qOXKKZwU0.net ち～ん（笑）     風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:47:31.53 ID:gG1ZTnnqa.net このユニフォームどこのや？     風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:48:56.17 ID:3P820ey9a.net 言うほどエロゲか？     風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:49:03.12 ID:CnWxatMv0.net スクワットわろた ってか動かないとアカンのかよ     風吹けば名無し＠＼(^o^)／:2016/09/21(水) 16:49:30.22"
+@page.site_name = "アニメ時代喋り場"
+@page.date = "2016/09/24"
 @page.save
 
 @page = Page.new
-@page.number = 74
-@page.title = "【朗報】VRのエロゲがヤバすぎるｗｗｗｗｗ : 暇人＼(^o^)／速報"
-@page.url = "http://himasoku.com/archives/51970885.html"
-@page.image = "/images/e0611cfc.png"
-@page.body = "  1: 風吹けば名無し＠＼(^o^)／  2016/09/21(水) 16:44:04.59 ID:rQTRCE/AM.net　 HTC Vive+カスタムメイド3D2を手にしたワイ pic.twitter.com/4jaC4k8erA— 水田ケンジ＠猛虎熊野と鈴谷さん委託中 (@suidengetsu) 2016年9月20日https://twitter.com/suidengetsu/sta"
-@page.site_name = "暇人＼(^o^)／速報"
-@page.date = "2016/09/22"
+@page.number = 83
+@page.title = "【悲報】東京ゲームショウ、VRでおっぱい揉み放題＆美少女と一緒に入浴体験とやりたい放題 : 妹はVIPPER"
+@page.url = "http://vipsister23.com/archives/8587496.html"
+@page.image = "/images/f9a5a632-s.jpg"
+@page.body = "1: 以下、＼(^o^)／でVIPがお送りします  投稿日：2016/09/15(木) 12:56:57.750 ID:PuJXFaZaa.net      "
+@page.site_name = "妹はVIPPER"
+@page.date = "2016/09/24"
 @page.save
 
 @page = Page.new
-@page.number = 73
-@page.title = "【朗報】VRのエロゲがヤバすぎるｗｗｗｗ : キニ速"
-@page.url = "http://blog.livedoor.jp/kinisoku/archives/4686651.html"
-@page.image = "/images/06361d3c.jpg"
-@page.body = "1：風吹けば名無し＠＼(^o^)／：2016/09/21(水) 16:44:04.59ID:rQTRCE/AM.net水田ケンジ＠猛虎熊野と鈴谷さん委託中@suidengetsuHTC Vive+カスタムメイド3D2を手にしたワイ https://t.co/4jaC4k8erA2016/09/20 23:13:24https://video.twimg.com/ext_tw_video/77823447320475"
-@page.site_name = "キニ速"
-@page.date = "2016/09/22"
+@page.number = 82
+@page.title = "【画像】東京ゲームショーでVRと実物人形のコラボでおっぱいモミモミ！！→人形壊れる・・・・ :【2ch】ニュー速クオリティ"
+@page.url = "http://news4vip.livedoor.biz/archives/52183636.html"
+@page.image = "/images/97dd603e.jpg"
+@page.body = "1:  ドラゴンスリーパー(千葉県)＠＼(^o^)／ 2016/09/16(金) 20:49:06.87 ID:ID:hXdCSlJ+0.net   ソース     引用元: 東京ゲームショーでVRと実物人形のコラボでおっぱいモミモミ！！→人形壊れる・・・・"
+@page.site_name = "【2ch】ニュー速クオリティ"
+@page.date = "2016/09/24"
+@page.save
+
+@page = Page.new
+@page.number = 81
+@page.title = "女性目線でみる「アダルトVRエキスポ」体験レポート | 日刊SPA!"
+@page.url = "http://nikkan-spa.jp/1203670"
+@page.image = "/images/advrex013.jpg"
+@page.body = "今年の6月、日本初のアダルトVRイベント「アダルトVRフェスタ」が開催されました。しかし、客数が予想を遥かに上回って会場前に殺到。残念ながら、開催直前で休止と延期が発表されたんですよね。そして、あれ…"
+@page.site_name = "日刊SPA!"
+@page.date = "2016/09/24"
+@page.save
+
+@page = Page.new
+@page.number = 80
+@page.title = "【動画】VRのエロゲがヤバすぎるｗｗｗｗｗ :【2ch】ニュー速クオリティ"
+@page.url = "http://news4vip.livedoor.biz/archives/52184418.html"
+@page.image = "/images/84ad1e22-s.jpg"
+@page.body = "1: 以下、ニュー速クオリティでお送りします 2016/09/21(水) 16:44:04.59 ID:ID:rQTRCE/AM.net https://twitter.com/suidengetsu/status/778235617087938560 引用元: ・【朗報】VRのエロゲがヤバすぎるwwwww"
+@page.site_name = "【2ch】ニュー速クオリティ"
+@page.date = "2016/09/24"
+@page.save
+
+@page = Page.new
+@page.number = 79
+@page.title = "アダルトVRフェスタで美少女がTENGAを何本売れるのか検証した話 - いつまでもアフタースクール"
+@page.url = "http://www.buntadayo.com/entry/tenga"
+@page.image = "/images/DSC01089-2.jpg"
+@page.body = "TENGAを手にした美少女に釣られてやってきた男性諸君！ごきげんよう文太です。 なんだこの状況...と思われるでしょうが、こちらは弊社が独自に開発した「美少女TENGAお持ち帰りサービス」です。 さて、何を言っているのかさっぱりわからない方の為に事の発端から説明していきましょう！"
+@page.site_name = "いつまでもアフタースクール"
+@page.date = "2016/09/24"
 @page.save
 
